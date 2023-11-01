c84d87068446ddaf9a42b745d66bd709e3b3ac9b
Madeliene <madelienestiller@gmail.com>
2023-11-01 13:53:25 +1000

    mini project complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    buildToDo(event.target.newtaskdescription.value);
+    document.querySelector('#newtaskdescription').value = null
+  });
+}); 
+
+
+function buildToDo(todo) {
+  console.log("task",todo)
+  let p = document.createElement('li')
+  let button = document.createElement('button')
+  button.addEventListener('click', handleDelete)
+  button.textContent = "x"
+  p.textContent = `${todo} `
+  p.appendChild(button)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove();
+}
